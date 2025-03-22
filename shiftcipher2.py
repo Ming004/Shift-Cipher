31d8e64e39b630880518059fca722c5d2815bffe
Ho Kin Ming Charles <109850421+Ming004@users.noreply.github.com>
2025-03-22 08:58:37 +0800

    Update shiftcipher2.py

--- shiftcipher2.py
+++ shiftcipher2.py
@@ -2,14 +2,14 @@
 #shift = int(input("Enter: "))
 
 def encrypt_check(raw):
-    return "valid" if len(raw.split()) >= 200 else " must be 200 words"
-    
+    return "valid" if len(raw.split()) >= 200 else "Text must be at least 200 words"
+
 def encrypt_cipher(raw, shift):
-    upper ='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    lower ='abcdefghijklmnopqrstuvwxyz'
-    shift_upper=upper[shift:]+upper[0:shift] # we don't use % 26 because we are using the same length of the alphabet
-    shift_lower=lower[shift:]+lower[0:shift]
-    table = str.maketrans(upper+lower, shift_upper+shift_lower) # translate table
+    upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    lower = 'abcdefghijklmnopqrstuvwxyz'
+    shift_upper = upper[shift % 26:] + upper[:shift % 26]
+    shift_lower = lower[shift % 26:] + lower[:shift % 26]
+    table = str.maketrans(upper + lower, shift_upper + shift_lower)
     return raw.translate(table)
 
 def decrypt(raw, shift):
@@ -17,6 +17,10 @@ def decrypt(raw, shift):
 
 def cal_word_count_of_cipher(raw):
     count = {}
-    for i in raw: count[i.lower()] = count.get(i.lower(), 0) + 1 if i.isalpha() else count.get(i, 0)
+    for i in raw:
+        if i.isalpha():
+            key = i.lower()
+        else:
+            key = i
+        count[key] = count.get(key, 0) + 1
     return count
-
